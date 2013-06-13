ced3d70871333124d2f8e794a3f657f56c9c9bf9
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-06-13 19:58:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cancancan::Application.config.secret_token = 'fa14d6c25a9f7c9e21419bdf326270bbafe062591fed7ef8431e9ed4607f4f450cd1238153b59fadd498fe2fe973aa53f288c3e3760a00a0f812acf75a4a8ebd'
